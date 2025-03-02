45c24f1fac04a2885baba9550a6e79ca092ba12b
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-03-03 00:13:25 +0200

    ок

---
+++ Homworkt/hw-core3.py
@@ -0,0 +1,9 @@
+
+# 1)є строка:
+# greeting = 'Hello, world'
+# записати кожний символ як окремий елемент списку і зробити його заглавним:
+# ['H', 'E', 'L', 'L', 'O', ',', ' ', 'W', 'O', 'R', 'L', 'D']
+
+greeting = 'Hello, world'
+i=greeting
+print(st.upper(i,','))
